9a4a07b21a5419a93cfb7fb7dcc1f0514590663f
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-11-22 14:15:03 +0200

    Initialize repo

---
+++ Testing/TestClass.java
@@ -0,0 +1,8 @@
+import org.Project.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClass { // change name of class when make test
+
+}
