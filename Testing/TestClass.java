2d23bbfa4b1c37e3f64420b457c9129557915292
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-12-13 21:57:42 +0200

    Remove Classes Folder and Testing Folder

--- Testing/TestClass.java
+++
@@ -1,8 +0,0 @@
-import org.Project.*;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class TestClass { // change name of class when make test
-
-}
--- classes/Main.java
+++
@@ -1,7 +0,0 @@
-package org.Project;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
