2de5c5dfa00c783f2c8d27a356bf3b4122495414
Gemmy <131499327+Mohamedgamal235@users.noreply.github.com>
2024-12-23 23:15:15 +0200

    Update ReportController.java

--- Learning-Management-Sys (4)/Learning-Management-Sys/Learning-Managment-System/src/main/java/com/lms/Learning_Managment_System/Controller/ReportController.java
+++ Learning-Management-Sys (4)/Learning-Management-Sys/Learning-Managment-System/src/main/java/com/lms/Learning_Managment_System/Controller/ReportController.java
@@ -1,10 +1,14 @@
 package com.lms.Learning_Managment_System.Controller;
 
 import com.lms.Learning_Managment_System.Service.GradeReportService;
+import com.lms.Learning_Managment_System.Service.courseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.lms.Learning_Managment_System.Model.* ;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reports")
@@ -13,13 +17,38 @@ public class ReportController {
     @Autowired
     private GradeReportService gradeReportService;
 
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private courseService courseservice;
+
     // Endpoint to generate grades report
     @PostMapping("/quiz/grades")
     public ResponseEntity<String> generateGradesReport(@RequestParam String courseTitle,
-                                                       @RequestParam int instructorId,
+                                                       @RequestParam int userID,
                                                        @RequestParam String fileName) {
+        boolean isInstructor = userController.getLoggedInInstructors().containsValue(userID);
+        boolean isAdmin = userController.getLoggedInAdmins().containsValue(userID);
+
+        if (!isInstructor && !isAdmin) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Access Denied: You must be a logged-in instructor or admin to add a new course.");
+        }
+
         try {
-            String filePath = gradeReportService.generateGradeReportForCourse(courseTitle, instructorId, fileName);
+            List<course> crss = courseService.getAllCourses();
+            int instructorID = -1 ;
+            for (course crs : crss){
+                if (crs.getCourse_title().equals(courseTitle)){
+                    instructorID = crs.getInstructor_id();
+                }
+            }
+            if (instructorID == -1)
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body("This instructor not on course ");
+
+            String filePath = gradeReportService.generateGradeReportForCourse(courseTitle, instructorID, fileName);
             return new ResponseEntity<>("Grades report generated: " + filePath , HttpStatus.OK);
         }
         catch (Exception e) {
@@ -42,10 +71,30 @@ public class ReportController {
 
     @PostMapping("/assignments/grades")
     public ResponseEntity<String> generateAssignmentGradesReport(@RequestParam String courseTitle,
-                                                                 @RequestParam int instructorId,
+                                                                 @RequestParam int userID,
                                                                  @RequestParam String fileName) {
+
+        boolean isInstructor = userController.getLoggedInInstructors().containsValue(userID);
+        boolean isAdmin = userController.getLoggedInAdmins().containsValue(userID);
+
+        if (!isInstructor && !isAdmin) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Access Denied: You must be a logged-in instructor or admin to add a new course.");
+        }
+
+        List<course> crss = courseService.getAllCourses();
+        int instructorID = -1 ;
+        for (course crs : crss){
+            if (crs.getCourse_title().equals(courseTitle)){
+                instructorID = crs.getInstructor_id();
+            }
+        }
+        if (instructorID == -1)
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("This instructor not on course ");
+
         try {
-            String filePath = gradeReportService.generateAssignmentGradesReport(courseTitle, instructorId, fileName);
+            String filePath = gradeReportService.generateAssignmentGradesReport(courseTitle, instructorID, fileName);
             return new ResponseEntity<>("Assignment Grades Report generated: " + filePath, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
