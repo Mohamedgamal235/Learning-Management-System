c884ea77770016e4c01fe0c9f111d77d449325ef
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-20 06:51:12 +0300

    Update Class

---
+++ Learning-Management-System/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿namespace Learning_Management_System.Models
+{
+    public class Enrollment
+    {
+        public Guid StudentId { get; set; }
+        public virtual Student? Student { get; set; }
+
+        public Guid CourseId { get; set; }
+        public virtual Course? Course { get; set; }
+
+        public DateTime EnrolledAt { get; set; }
+        public double ProgressPercent { get; set; }
+        public double Grade { get; set; }
+        public DateTime LastAccessed {  get; set; } 
+        public int AttendanceCount { get; set; }
+    }
+}
