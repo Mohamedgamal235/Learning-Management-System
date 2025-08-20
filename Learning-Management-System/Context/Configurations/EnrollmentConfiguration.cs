d7841edb4662e35514963a2923a5dd7f3bef105c
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-21 01:10:06 +0300

    Make Configuration of Enrollment

--- Learning-Management-System/Context/Configurations/EnrollmentConfiguration.cs
+++ Learning-Management-System/Context/Configurations/EnrollmentConfiguration.cs
@@ -1,6 +1,26 @@
-﻿namespace Learning_Management_System.Context.Configurations
+﻿using Learning_Management_System.Models;
+
+namespace Learning_Management_System.Context.Configurations
 {
-    public class EnrollmentConfiguration
+    public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
     {
+        public void Configure(EntityTypeBuilder<Enrollment> builder)
+        {
+            builder.HasKey(e => new { e.CourseId, e.StudentId });
+
+            builder.Property(e => e.EnrolledAt).IsRequired();
+            builder.Property(e => e.ProgressPercent).IsRequired();
+            builder.Property(e => e.AttendanceCount).IsRequired();
+
+            builder.HasOne(e => e.Course)
+                .WithMany(e => e.Enrollments)
+                .HasForeignKey(e => e.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.Student)
+                .WithMany(e => e.Enrollments)
+                .HasForeignKey(e => e.StudentId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
