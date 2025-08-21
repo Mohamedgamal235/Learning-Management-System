4b8d9ac030f5af26522d01b32334ec3ead91743d
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-22 02:45:17 +0300

    Add Validation of Linked in link

---
+++ Learning-Management-System/Models/Attributes/LinkedInUrlAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learning_Management_System.Models.Attributes
+{
+    public class LinkedInUrlAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+                return ValidationResult.Success;
+
+            string link = value.ToString();
+            if (link.Contains("linkedin.com" , StringComparison.OrdinalIgnoreCase))
+                return ValidationResult.Success;
+
+            return new ValidationResult("Shoul Contain Linkedin.com");
+        }
+    }
+}
