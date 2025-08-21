ba7c19ce39252dfd78f59a8a396706934dec1360
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-22 00:45:41 +0300

    Update Validation

--- Learning-Management-System/Models/User.cs
+++ Learning-Management-System/Models/User.cs
@@ -1,13 +1,24 @@
 ﻿
+using Learning_Management_System.Models.Attributes;
+using System.ComponentModel.DataAnnotations;
+
 namespace Learning_Management_System.Models
 {
     public abstract class User
     {
         public Guid UserId { get; set; }
         public string UserName { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [PasswordComplexity]
         public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password not match")]
         public string ConfirmedPassword { get; set; }
+
         public string? LinkedIn {  get; set; }
         public string? PhotoPath { get; set; }
         public Role Role { get; set; }
