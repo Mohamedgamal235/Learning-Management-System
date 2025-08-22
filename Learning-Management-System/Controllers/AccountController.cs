97e32cb2397fea77e39e8d7d8bcb23de356d9839
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-23 01:29:36 +0300

    Initial Controller

---
+++ Learning-Management-System/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Learning_Management_System.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learning_Management_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountService accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpGet]
+        public IActionResult Register(RegisterModel registerModel)
+        {
+            return View();
+        }
+
+    }
+}
