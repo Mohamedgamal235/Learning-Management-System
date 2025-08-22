93ebfe5e740bbc2cea42ddb8a9d3eb20be0273bc
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-23 01:27:39 +0300

    Implement Account Service

--- Learning-Management-System/Service/AccountService.cs
+++ Learning-Management-System/Service/AccountService.cs
@@ -1,6 +1,74 @@
-﻿namespace Learning_Management_System.Service
+﻿using Learning_Management_System.Repository.Interfaces;
+using Learning_Management_System.Service.Interfaces;
+
+namespace Learning_Management_System.Service
 {
-    public class AccountService
+    public class AccountService : IAccountService
     {
+        private readonly IAccountRepository accountRepository;
+
+        public AccountService(IAccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+        public async Task<SignInResult> LoginAsync(LoginModel login)
+        {
+            var user = await accountRepository.FindByEmailAsync(login.Email);
+            if (user == null) 
+                return SignInResult.Failed;
+            return await accountRepository.PasswordSignInAsync(user , login.Password , login.RememberMe);
+        }
+
+        public async Task LogoutAsync()
+        {
+            await accountRepository.SignOutAsync();
+        }
+
+        public async Task<IdentityResult> RegisterAsync(RegisterModel registerModel)
+        {
+            AppUser user; 
+            if (registerModel.Role == Role.Student)
+            {
+                user = new Student
+                {
+                    FirstName = registerModel.FirstName,
+                    MiddelName = registerModel.MiddelName,
+                    LastName = registerModel.LastName,
+                    Email = registerModel.Email,
+                    UserName = registerModel.Email,
+                    Role = Role.Student,
+                    School = null,
+                    Headline = null,
+                    EnrollmentDate = DateTime.Now,
+                }; 
+            }
+            else if (registerModel.Role == Role.Instructor)
+            {
+                user = new Instructor
+                {
+                    FirstName = registerModel.FirstName,
+                    MiddelName = registerModel.MiddelName,
+                    LastName = registerModel.LastName,
+                    Email = registerModel.Email,
+                    UserName = registerModel.Email,
+                    Role = Role.Instructor,
+                    JopTitle = null,
+                    Salary = null,
+                }; 
+            }
+            else
+                throw new Exception("Invalid role");
+
+            var result = await accountRepository.CreateUserAsync(user, registerModel.Password);
+
+            if (result.Succeeded)
+            {
+                if (user is Student student)
+                   await accountRepository.AddStudentAsync(student);
+                else if (user is Instructor instructor)
+                    await accountRepository.AddInstructorAsync(instructor);
+            }
+            return result; 
+        }
     }
 }
