07e87dc843fb7e1cf2f23df69ddc1b497b864883
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-22 20:43:56 +0300

    Update for identity user

---
+++ Learning-Management-System/Models/AppUser.cs
@@ -0,0 +1,37 @@
+﻿
+using Learning_Management_System.Models.Attributes;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learning_Management_System.Models
+{
+    public abstract class User : IdentityUser<Guid>
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [PasswordComplexity]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password not match")]
+        public string ConfirmedPassword { get; set; }
+
+
+        [DataType(DataType.Url)]
+        [LinkedInUrl]
+        public string? LinkedIn {  get; set; }
+
+        [DataType(DataType.Upload)]
+        public string? PhotoPath { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+
+        public virtual List<Notification>? Notifications { get; set; }
+    }
+}
