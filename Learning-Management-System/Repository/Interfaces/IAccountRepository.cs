1a9c15af26491f2c60cb9ed44fdc4fdfaa7891e9
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-22 23:20:45 +0300

    Update Repository

---
+++ Learning-Management-System/Repository/Interfaces/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Learning_Management_System.Repository.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> CreateUserAsync(AppUser user, string password);
+        Task<AppUser?> FindByNameAsync(string username);
+        Task<bool> CheckPasswordAsync(AppUser user , string password);
+        Task AddToRoleAsync(AppUser user, string role);
+    }
+}
