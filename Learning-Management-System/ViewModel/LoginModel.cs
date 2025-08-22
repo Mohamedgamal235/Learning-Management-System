54f4d0c259099a1e5a0eb43bd5833d6fd111fc7e
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-23 00:29:42 +0300

    Add Models for Views

---
+++ Learning-Management-System/ViewModel/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using Learning_Management_System.Models.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learning_Management_System.ViewModel
+{
+    public class LoginModel
+    {
+        [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@gmail\.com$", ErrorMessage = "Only Gmail Address is allowed.")]
+        [Required] public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Learning-Management-System/ViewModel/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using Learning_Management_System.Models.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Learning_Management_System.ViewModel
+{
+    public class RegisterModel
+    {
+        [Required] public string FirstName { get; set; }
+        [Required] public string MiddelName { get; set; }
+        [Required] public string LastName { get; set; }
+
+        [EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@gmail\.com$", ErrorMessage = "Only Gmail Address is allowed.")]
+        [Required] public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [PasswordComplexity]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password not match")]
+        public string ConfirmedPassword { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+}
